97ef9019a39858a3fd88cdc8e4e617d6fece86c1
Mario Bezerra <97652522+Mario-Bezerra@users.noreply.github.com>
2022-04-14 20:53:28 -0300

    Add files via upload

---
+++ Endereco.java
@@ -0,0 +1,43 @@
+
+public class Endereco {
+	
+	private String cep;
+	private String cidade;
+	private String rua;
+	private String numero;
+	private String complemento;
+	
+	
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getCidade() {
+		return cidade;
+	}
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	public String getRua() {
+		return rua;
+	}
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+	public String getNumero() {
+		return numero;
+	}
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	public String getComplemento() {
+		return complemento;
+	}
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+	
+	
+}
